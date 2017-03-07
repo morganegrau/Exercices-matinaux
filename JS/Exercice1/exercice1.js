46c384fbecd9d83932f9a651c861fab83a143048
Morgane Grau <mogrba@gmail.com>
2017-03-07 12:33:29 +0100

    suite exercice 1

--- JS/Exercice1/exercice1.js
+++ JS/Exercice1/exercice1.js
@@ -2,14 +2,30 @@ var tabImg =['jaune.jpg','vert.jpg','rouge.jpg'];
 
 DIV = document.getElementById('div');
 
-for (var i = 0; i<tabImg.length; i++) { 
-	DIV.innerHTML += "<img src="+tabImg[i]+">";
-}
+// for (var i = 0; i<tabImg.length; i++) { 
+// 	DIV.innerHTML += "<img src= "+tabImg[i]+">";
+// }
 
-function sup() {
-       tabImg.pop();
-       DIV.innerHTML ="";
-for (var i = 0; i<tabImg.length; i++) { 
-	DIV.innerHTML += "<img src="+tabImg[i]+">";
-}
-}
+// function sup() {
+//        tabImg.pop();
+//        DIV.innerHTML ="";
+// for (var i = 0; i<tabImg.length; i++) { 
+// 	DIV.innerHTML += "<img src="+tabImg[i]+">";
+// }
+// }
+
+
+function getinput()
+{
+var input = document.getElementById("input").value;
+	if (input > 0 && input < 20) {
+		DIV.innerHTML += "<img src="+tabImg[2]+">"
+	}
+	else if (input < 0) {
+		DIV.innerHTML += "<img src="+tabImg[1]+">"
+	}
+	else {
+		DIV.innerHTML += "<img src="+tabImg[0]+">"
+	}
+} 
+
