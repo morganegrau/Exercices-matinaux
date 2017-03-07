0314d689df11130436a3300de80065a97bec9350
Morgane Grau <mogrba@gmail.com>
2017-03-07 16:03:30 +0100

    avancement exercice

--- JS/Exercice1/exercice1.js
+++ JS/Exercice1/exercice1.js
@@ -1,5 +1,5 @@
 var tabImg =['jaune.jpg','vert.jpg','rouge.jpg'];
-
+var myArray = [];
 DIV = document.getElementById('div');
 
 // for (var i = 0; i<tabImg.length; i++) { 
@@ -27,5 +27,26 @@ var input = document.getElementById("input").value;
 	else {
 		DIV.innerHTML += "<img src="+tabImg[0]+">"
 	}
-} 
+} 129
 
+
+
+// function adinput(){
+// var input = document.getElementById("input").value;
+// 	if (input != null){
+// 	myArray.push(input);
+// 	document.getElementById('recup').innerHTML = myArray.sort();
+// 	}
+// }
+
+
+function adinput() {
+	var input = document.getElementById("input").value;
+	if (input != null){
+	myArray.push(input);
+    myArray.sort(function(a, b){return a-b});
+    document.getElementById('recup').innerHTML = myArray;
+	document.getElementById('afficher').innerHTML =(myArray.length[0] && myArray.length[-1])
+
+	}
+}
